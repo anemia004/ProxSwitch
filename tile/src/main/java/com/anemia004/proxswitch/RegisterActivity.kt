d290f1ce49e61b97387dffbc0b66d41f0072fa6c
anemia004 <Khorramshahr04@duck.com>
2026-07-09 23:26:29 +0530

    Create RegisterActivity.kt

---
+++ tile/src/main/java/com/anemia004/proxswitch/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package com.anemia004.proxswitch
+
+import android.app.Activity
+import android.os.Bundle
+
+class RegisterActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        finish()
+    }
+}
